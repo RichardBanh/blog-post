114f2f21e20b796adb147c3755a7733126047c27
rbanh <rbanh@me.com>
2019-05-27 17:45:33 -0700

    method 2 --action in actio n

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,5 +1,12 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
-import _ from 'lodash';
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)))
+};
+
 export const fetchPosts = () => {
   return async dispatch => {
     const response = await jsonPlaceholder.get("/posts");
@@ -8,13 +15,8 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = (id) => dispatch => {
-_fetchUser(id, dispatch)
-}
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-  dispatch({type: 'FETCH_USER', payload: response.data})
-
-})
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import {fetchUser} from '../actions'
+
 class UserHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId)
-  }
+
   render() { 
     const {user} = this.props
 
@@ -20,4 +18,4 @@ const mapStateTosProps = (state, ownProps) => {
     }
 }
  
-export default connect(mapStateTosProps, {fetchUser}) (UserHeader);
+export default connect(mapStateTosProps) (UserHeader);
